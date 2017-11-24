b9318e9727730abb4d017daf1408244ca4a7b6d2
Konstantin <eekkonstantin@gmail.com>
2017-11-24 14:46:09 +0800

    Stock class added

---
+++ objects/Stock.java
@@ -0,0 +1,61 @@
+public class Stock {
+  /**
+   * Total number of shares available
+   */
+  public static final int SUPPLY = 100;
+  public static final String NAME = "Apple";
+  public static final double MIN = 0.01;
+  public static final double MAX = 100.0;
+
+  /**
+   * Price of a single share
+   */
+  private double price;
+
+  /**
+   * Number of shares currently available
+   */
+  private int available;
+
+  @Override
+  public String toString() {
+    return NAME + ": " + available + " shares available at $" + price + " each.";
+  }
+
+  /**
+   * Changes the price of each share.
+   * @param double change Amount to change the price by.
+   */
+  public void changePrice(double change) {
+    price += change;
+    switch (price) {
+      case (> MAX):
+        price = MAX;
+        break;
+      case (< MIN):
+        price = MIN;
+        break;
+    }
+  }
+
+  /**
+   * Reduces the number of shares available. Returns false with no modifications
+   * to supply if no more shares are available.
+   * @return Whether a share could be succesfully sold.
+   */
+  public boolean sold() {
+    if (available > 0) {
+      available--;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Increases the supply of shares by one.
+   */
+  public void returned() {
+    available++;
+  }
+
+}
