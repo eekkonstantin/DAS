84846b11ae014a93fb05765d101ebcbe05ebf542
seahcy <33949461+seahcy@users.noreply.github.com>
2017-11-24 15:22:05 +0800

    cool RMI files

---
+++ StocksServerImpl.java
@@ -0,0 +1,39 @@
+import java.rmi.RemoteException;
+import java.rmi.server.RemoteServer;
+import java.rmi.server.ServerNotActiveException;
+import java.util.Random;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+public class StocksServerImpl extends java.rmi.server.UnicastRemoteObject implements StocksServer
+{
+    static ArrayList<String> IPAddresses = new ArrayList<String>();
+    
+	protected StocksServerImpl() throws RemoteException
+    {
+		super();
+	}
+
+	public long[] syncTime() throws RemoteException
+    {
+		long[] time = new long[2];
+		time[0] = System.currentTimeMillis() ;
+		try
+        {
+			TimeUnit.SECONDS.sleep(1);
+			time[1] = System.currentTimeMillis();
+		}
+        catch (InterruptedException e)
+        {
+			e.printStackTrace();
+		}
+		return time;
+	}
+
+	public String getIP() throws RemoteException, ServerNotActiveException
+    {
+		String IPAddress = RemoteServer.getClientHost();
+		IPAddresses.add(IPAddress);
+		return IPAddress;
+	}
+
+}
